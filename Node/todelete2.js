9c4d33f0115066cf085768f24490acbf8a9009ac
joeli <joelimgu@gmail.com>
2020-08-12 17:27:46 +0200

    0.0.23

--- Node/todelete2.js
+++ Node/todelete2.js
@@ -1,20 +1,25 @@
 
+const job = {
+  name: "TS0001",
+  ref: "TS0001",
+  pre_url: "https://tictap.me/track/",
+  qty: "11",
+  qtyDone: "0",
+  rom: "0",
+  status: "start"
+};
 
-var machine = {
-    status: "off",
-    databaseConnected : false,
-    arduinoConnected : false,
-    lastJobDone : undefined,
-    currentJob : undefined
-  }
 
-machine.status = "ok"
+const query = "INSERT INTO jobs(\`name\`,ref,pre_url,uid_len,qty,qtydone,rom,\`status\`,modified_at) VALUES("
+  + job.name +","
+  + job.ref + ","
+  + job.pre_url + ","
+  + job.qty + ","
+  + job.qtyDone + ","
+  + job.rom + ","
+  + job.status + ","
+  + Date.now()
+  + ");"
 
 
-function getStatus(){
-  console.log("called getStatus");
-  return machine.status;
-}
-
-console.log(machine.status);
-module.exports = {getStatus};
+console.log(query);
